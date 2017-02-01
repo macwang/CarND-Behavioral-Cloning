968205d85be54ff5be0ea1ad3adec30b34cee45b
Mac Wang <macwang1@gmail.com>
2017-02-01 21:55:15 +0800

    add 1st implementation

---
+++ helper.py
@@ -0,0 +1,69 @@
+import os
+
+import pandas as pd
+import numpy as np
+import cv2
+import json
+import errno
+
+def preprocessing(img):
+    # crop 160x320 ==> 99x300
+    img_tmp = img[41:139, 10:310, :]
+
+    # resize 99x300 ==> 66x200
+    img_tmp = cv2.resize(img_tmp, (200, 66))
+
+    # convert it to YUV
+    img_tmp = cv2.cvtColor(img_tmp, cv2.COLOR_BGR2YUV)
+
+    # normalize
+    img_tmp = img_tmp / 128.0 - 1.0
+
+    return img_tmp
+
+def generate_arrays_from_file(log_csv, batch_size=32):
+    driving_log = pd.read_csv(os.path.join('data', log_csv))
+    num_of_samples = len(driving_log)
+    while True:
+            X = []
+            y = []
+
+            for i in range(batch_size):
+                # select idx
+                idx = np.random.randint(0, num_of_samples)
+                # select camera, 0 center, 1 left, 2 right
+                camera = np.random.randint(0, 3)
+
+                filename = driving_log.iloc[idx][camera].strip()
+                steering = driving_log.iloc[idx]['steering']
+                if camera == 1:
+                    steering += 0.15
+                elif camera == 2:
+                    steering -= 0.15
+                img = cv2.imread(os.path.join('data', filename))
+                img = preprocessing(img)
+                # select mirror
+                if np.random.randint(0, 2) == 1:
+                    img = img[:, ::-1, :]
+                    steering = -steering
+
+                X.append(img)
+                y.append(steering)
+            assert len(X) == batch_size
+            yield np.array(X), np.array(y)
+
+def delete_file(filename):
+    try:
+        os.remove(filename)
+    except OSError as error:
+        if error.errno != errno.ENOENT:
+            raise
+
+def save_model(model, json_file='model.json', h5_file='model.h5'):
+    delete_file(json_file)
+    delete_file(h5_file)
+
+    json_string = model.to_json()
+    with open(json_file, 'w') as outfile:
+        json.dump(json_string, outfile)
+    model.save_weights(h5_file)
---
+++ model.py
@@ -0,0 +1,30 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers.core import Flatten
+from keras.layers.convolutional import Convolution2D
+
+import helper
+
+# model start
+model = Sequential()
+model.add(Convolution2D(24, 5, 5, border_mode='valid', activation='relu', subsample=(2, 2), input_shape=(66, 200, 3)))
+model.add(Convolution2D(36, 5, 5, border_mode='valid', activation='relu', subsample=(2, 2)))
+model.add(Convolution2D(48, 5, 5, border_mode='valid', activation='relu', subsample=(2, 2)))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu'))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu'))
+model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1))
+model.compile('adam', 'mse')
+# model end
+
+history = model.fit_generator(helper.generate_arrays_from_file('driving_log.csv'), 128, 1)
+
+helper.save_model(model)
